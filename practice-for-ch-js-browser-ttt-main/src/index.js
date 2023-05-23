6670e7399e714c95a09dc1bc0416f131ae161675
misha <mishabansal7@gmail.com>
2023-05-23 15:17:13 -0700

    finished tic tac toe

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,5 +1,14 @@
-Add your import statements for View and Game here
+// Add your import statements for View and Game here
+import View from "./ttt-view";
+import Game from "../ttt_node/game.js";
+window.Game = Game;
+window.View = View;
+
+console.log("tic tac toe game")
 
 document.addEventListener("DOMContentLoaded", () => {
+  const view_container = document.getElementsByClassName('ttt')[0]
+  const game = new Game();
+  const view = new View(game, view_container);
   // Your code here
 });
--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,17 +1,47 @@
 class View {
   constructor(game, el) {
+    this.grid = this.setupBoard();
+    this.game = game;
+    el.appendChild(this.grid);
   }
   
   setupBoard() {
+    let grid = document.createElement("ul")
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        let space = document.createElement("li")
+        space.dataset.row = i;
+        space.dataset.col = j;
+        grid.appendChild(space);
+      }
+    }
+    let newHandleClick = this.handleClick.bind(this)
+    grid.addEventListener('click', newHandleClick);
+    return grid;
   }
   
   handleClick(e) {
-  }
-
-  makeMove(square) {
+    // console.log(this)
+    let row = e.target.dataset.row;
+    let col = e.target.dataset.col;
+    let pos = [row, col]
+    // if () {
+    //   alert(MoveError)
+    // } else {
+    this.game.playMove(pos)
+    e.target.classList.add("marked");
+    e.target.innerText = this.game.currentPlayer;
+    this.handleGameOver();
+    // }
   }
   
   handleGameOver() {
+    console.log(this.game.currentPlayer)
+    if (this.game.winner()) {
+      alert (`player ${this.game.currentPlayer} wins!`)
+    } else if (this.game.isOver()) {
+      alert (`is a draw`);
+    }
   }
 }
 
